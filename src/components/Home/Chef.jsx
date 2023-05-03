3fdeafd1a7da504af792e997ad566c306c154572
Md. pieash ahmed <81944951+pieash9@users.noreply.github.com>
2023-05-03 11:50:20 +0600

    complete chef section

--- src/components/Home/Chef.jsx
+++ src/components/Home/Chef.jsx
@@ -10,14 +10,16 @@ const Chef = () => {
       .then((data) => setChefsData(data));
   }, []);
   return (
-    <div>
-        <div>
-            <h3 className="text-3xl text-center ">Chef Details</h3>
-        </div>
-      <div>
-      {chefsData.map((singleData) => (
-        <ChefCard key={singleData.id} singleData={singleData} />
-      ))}
+    <div className="mt-36 my-container">
+      <div className="mb-10">
+        <h3 className="text-4xl text-center font-bold text-gray-600 mb-4">Our Chef</h3>
+        <p className="md:w-2/3 mx-auto px-2 text-lg text-center text-gray-600">Meet our renowned chef and learn about their culinary expertise, from classic Italian dishes to modern fusion creations. Discover their story and passion for cooking.</p>
+      </div>
+
+      <div className="grid md:grid-cols-3 gap-10">
+        {chefsData.map((singleData) => (
+          <ChefCard key={singleData.id} singleData={singleData} />
+        ))}
       </div>
     </div>
   );
--- src/components/Home/ChefCard.jsx
+++ src/components/Home/ChefCard.jsx
@@ -1,14 +1,34 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable react/prop-types */
-import React from 'react';
+import React from "react";
+import { FcLike } from "react-icons/fc";
 
-const ChefCard = ({singleData}) => {
-    const {name} = singleData
-    return (
-        <div>
-            {name}
+const ChefCard = ({ singleData }) => {
+  const { name, likes, recipesNo, experience, picture } = singleData;
+  return (
+    <div className="card card-compact  bg-base-100 shadow-2xl mt-10">
+      <figure>
+        <img className="rounded-t-lg" src={picture} alt="Shoes" />
+      </figure>
+      <div className="card-body">
+        <h2 className="card-title !text-2xl text-gray-600">{name}</h2>
+        <p className="text-base text-gray-600 font-medium">
+          Experience: {experience} Years
+        </p>
+        <div className="flex items-center justify-between">
+          <p className="text-base text-gray-600 font-medium">
+            No of Recipes: {recipesNo}
+          </p>
+          <div className="flex items-center">
+          <FcLike className="text-xl mr-2"/> <span className="text-base text-gray-600 font-medium"> {likes}</span>
+          </div>
         </div>
-    );
+        <div className="card-actions justify-end">
+          <button className="button-primary text-white !py-2 my-5">View Recipes</button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ChefCard;
